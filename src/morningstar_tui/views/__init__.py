6ea3fa3026e92190ca3d1dda827104828ed8647a
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 15:40:26 -0700

    Export v0.9 reorganized workspaces

--- src/morningstar_tui/views/__init__.py
+++ src/morningstar_tui/views/__init__.py
@@ -1,8 +1,9 @@
-"""Dashboard content views from v0.1 through v0.8."""
+"""Dashboard content views from v0.1 through v0.9."""
 
 from morningstar_tui.views.command_palette import CommandChosen, CommandPaletteView
 from morningstar_tui.views.controller_detail import ControllerDetailView
 from morningstar_tui.views.controllers import ControllerActivated, ControllersView
+from morningstar_tui.views.diagnostics import DiagnosticsView
 from morningstar_tui.views.events import EventsView
 from morningstar_tui.views.fleet import FleetView
 from morningstar_tui.views.forecast import ForecastView
@@ -12,6 +13,9 @@ from morningstar_tui.views.investigation import InvestigationView
 from morningstar_tui.views.noc import NOCView
 from morningstar_tui.views.overview import OverviewView
 from morningstar_tui.views.power_flow import PowerFlowView
+from morningstar_tui.views.system_details import SystemDetailsView
+from morningstar_tui.views.telemetry import TelemetryView
+from morningstar_tui.views.topology import TopologyView
 
 __all__ = [
     "CommandChosen",
@@ -19,6 +23,7 @@ __all__ = [
     "ControllerActivated",
     "ControllerDetailView",
     "ControllersView",
+    "DiagnosticsView",
     "EventsView",
     "FleetView",
     "ForecastView",
@@ -28,4 +33,7 @@ __all__ = [
     "NOCView",
     "OverviewView",
     "PowerFlowView",
+    "SystemDetailsView",
+    "TelemetryView",
+    "TopologyView",
 ]
