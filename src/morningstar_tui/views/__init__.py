e1e95300b1a766121cf529b51494869c509ec136
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 15:27:51 -0700

    Export v0.6-v0.8 views

--- src/morningstar_tui/views/__init__.py
+++ src/morningstar_tui/views/__init__.py
@@ -1,7 +1,10 @@
-"""Dashboard content views from v0.1 through v0.5."""
+"""Dashboard content views from v0.1 through v0.8."""
 
-from morningstar_tui.views.controllers import ControllersView
+from morningstar_tui.views.command_palette import CommandChosen, CommandPaletteView
+from morningstar_tui.views.controller_detail import ControllerDetailView
+from morningstar_tui.views.controllers import ControllerActivated, ControllersView
 from morningstar_tui.views.events import EventsView
+from morningstar_tui.views.fleet import FleetView
 from morningstar_tui.views.forecast import ForecastView
 from morningstar_tui.views.history import HistoryView
 from morningstar_tui.views.incidents import IncidentsView
@@ -11,8 +14,13 @@ from morningstar_tui.views.overview import OverviewView
 from morningstar_tui.views.power_flow import PowerFlowView
 
 __all__ = [
+    "CommandChosen",
+    "CommandPaletteView",
+    "ControllerActivated",
+    "ControllerDetailView",
     "ControllersView",
     "EventsView",
+    "FleetView",
     "ForecastView",
     "HistoryView",
     "IncidentsView",
